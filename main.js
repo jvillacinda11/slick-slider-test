85247d6bec754b4eec198ecee20c94b948a820d6
jvillacinda <jvillacoding@gmail.com>
2023-11-14 09:34:53 -0800

    responsive. GA4 tracking upsale event logic

--- main.js
+++ main.js
@@ -38,3 +38,43 @@ $('.center').slick({
       }
     ]
   });
+
+
+document.addEventListener('click', function (event) {
+  if (event.target.matches('[name~="test"]')){
+
+    //make array
+    const upsolditems = [];
+    var numupsolditems = 0;
+    const checkboxes = document.querySelectorAll('input[name^="Upsell_Selected["]');
+
+    //loop to check?
+  checkboxes.forEach( checkbox =>{
+    if(checkbox.checked){
+      console.log("ping")
+      upsolditems.push({
+        itemName: checkbox.getAttribute('data-item-name')
+      })
+      numupsolditems = numupsolditems+1;
+    }
+  })
+
+  console.log(upsolditems);
+  console.log(numupsolditems);
+    //push to array
+
+
+    if(numupsolditems === 0 ){
+    //push event no sale
+console.log("event = upsale-failed")
+        
+    }
+    else{
+    //push event sale
+console.log("event = upsale")
+
+        
+    }
+    
+}
+})
